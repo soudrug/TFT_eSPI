58ff41abc1ca7384bb3a0ce47857f43e9dbbcf42
vlastahajek <vlastimil.hajek@bonitoo.io>
2019-02-24 12:56:15 +0100

    Added distinguishing Esp8266 vs Esp32

--- User_Setup_Select.h
+++ User_Setup_Select.h
@@ -18,8 +18,11 @@
                           //  platformio.ini, see notes in "Tools" folder.
 
 // Only ONE line below should be uncommented.  Add extra lines and files as needed.
-
+#ifdef ARDUINO_ARCH_ESP32
+#include <User_SetupEsp32.h>
+#else
 #include <User_Setup.h>           // Default setup is root library folder
+#endif
 
 //#include <User_Setups/Setup1_ILI9341.h>  // Setup file configured for my ILI9341
 //#include <User_Setups/Setup2_ST7735.h>   // Setup file configured for my ST7735
